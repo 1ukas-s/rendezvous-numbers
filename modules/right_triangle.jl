fe49be58887fc4832006a13cb73c86b74837a1cc
1ukas-s <115094913+1ukas-s@users.noreply.github.com>
2022-11-06 19:15:08 -0600

    Update right_triangle.jl
    Modified parameterization so that diameter is equal to 1 and takes an angle input between 0 and pi/4 as one of the angles so that any right triangle up to similarity can be solved for.

--- modules/right_triangle.jl
+++ modules/right_triangle.jl
@@ -1,19 +1,41 @@
 module_name = "right_triangle.jl -- by Lukas Stuelke."
 using Distributed
+ang = pi/4
+
+try
+    global ang
+    ang = parse(Float64, replace(replace(split(ARGS[end])[1],"["=>""),"]"=>""))
+    println("Right triangle with one angle = ", ang, ".")
+catch
+    println("\nPlease make the last argument to the command line a 1-value matrix with an angle ∈[0.0, pi/4], for example julia ./solver.jl 2 6 \'[0.392]\'")
+    exit()
+end
+if ang > pi/4 || ang < 0.0
+    println("\nPlease make the last argument to the command line a 1-value matrix with an angle ∈[0.0, pi/4], for example julia ./solver.jl 2 6 \'[0.392]\'")
+    exit()
+end
+if Distributed.nworkers() > 1
+    for i = 2:nworkers()+1
+        global ang
+        remotecall_fetch(()->ang, i)
+    end
+end
 Distributed.@everywhere begin
-	function find_diameter() # Finds the diameter of a regular polygon using symmetries.
-		return 2^(1/2)
+	function find_diameter() # Diameter defined to be 1 by our parameterization below
+		return 1.0
 	end
 
 	function param(a)
-		m = 2+2^(1/2)
-		a = a%1
+		global ang
+		csa = cos(ang)
+		sna = sin(ang)
+		m = csa+sna+1
 		if a <= 0 || a >= 1
 			toReturn = (0, 0)
-		elseif a <= 1/m
+		elseif a <= csa/m
 			toReturn = (m*a, 0)
-		elseif a <= (m-1)/m
-			toReturn = (-(m-1)*(a+1/m-1), (m-1)*(a-1/m))
+		elseif a <= (csa+1)/m
+			toReturn = (csa*(1+csa-m*a), sna*(m*a-csa))
 		else
 			toReturn = (0, m*(1-a))
 		end
